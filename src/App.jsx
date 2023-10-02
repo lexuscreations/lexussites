e82506457ada7d081973b217a9196415590e14de
LokeshVishwakarma <lokeshvishwakarma2611@gmail.com>
2023-10-02 17:37:37 +0530

    updated-with-real-links-and-screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,16 @@
-import { useState } from "react";
+import Card from "./components/Card";
 import { sitesDataArrObj } from "./config";
-import { IoOpenOutline } from "react-icons/io5";
-import { MdOutlineOpenInBrowser } from "react-icons/md";
-
-const Card = ({ img, hosted, siteName, url }) => {
-  const [isOpened, setIsOpened] = useState(false);
-
-  return (
-    <div>
-      <div className="rounded-lg overflow-hidden h-52">
-        {isOpened ? (
-          <iframe src={url} height={"100%"} width={"100%"} />
-        ) : (
-          <img src={img} alt="" className="" />
-        )}
-      </div>
-
-      <div className="flex justify-between mt-3 text-slate-600 font-medium">
-        <h3>
-          {siteName.length > 18 ? `${siteName.substr(0, 18)}...` : siteName}
-        </h3>
-        <p>{hosted}</p>
-      </div>
-
-      <div>
-        <a
-          href="#"
-          className="text-slate-400 flex items-center gap-1"
-          onClick={() => setIsOpened((prev) => !prev)}
-        >
-          <MdOutlineOpenInBrowser className={isOpened ? "rotate-180" : ""} />
-          {isOpened ? "Close" : "Open"}{" "}
-        </a>
-        <a
-          href={url}
-          target="_blank"
-          className="text-slate-400 flex items-center gap-1"
-        >
-          <IoOpenOutline />
-          Visit
-        </a>
-      </div>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div className="h-screen bg-slate-100 flex justify-center flex-col overflow-auto">
-      <div className="my-3 w-5/6 mx-auto">
+      <div className="my-3 w-11/12 sm:w-5/6 mx-auto">
         <h2 className="text-xl font-medium text-slate-500">
           LexusSite's | {sitesDataArrObj.length}
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-5/6 h-4/5 overflow-auto mx-auto border bg-slate-50 p-12 rounded-lg border-slate-300">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full sm:w-5/6 h-full sm:h-4/5 overflow-auto mx-auto border bg-slate-50 p-2 sm:p-12 rounded-lg border-slate-300">
         {sitesDataArrObj.map((item, index) => (
           <Card
             key={index}
@@ -62,6 +18,7 @@ const App = () => {
             hosted={item.hosted}
             siteName={item.siteName}
             url={item.url}
+            github={item.github}
           />
         ))}
       </div>
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { IoOpenOutline } from "react-icons/io5";
+import { MdOutlineOpenInBrowser } from "react-icons/md";
+
+const Card = ({ img, hosted, siteName, url, github }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+    setHasError(false);
+  };
+
+  const handleError = () => {
+    setIsLoading(false);
+    setHasError(true);
+  };
+
+  return (
+    <div className="h-48 flex flex-col justify-between">
+      <div className="h-32 flex justify-center items-center">
+        {isOpened ? (
+          <>
+            {isLoading && (
+              <p className="text-slate-600 text-xl font-medium">Loading...</p>
+            )}
+            {hasError && (
+              <p className="text-red-500 text-xl font-medium">Error</p>
+            )}
+            <iframe
+              src={url}
+              className={`h-full ${isLoading || hasError ? "hidden" : ""}`}
+              width={"100%"}
+              onLoad={handleLoad}
+              onError={handleError}
+            />
+          </>
+        ) : (
+          <img src={img} alt="siteImage h-full" className="rounded-lg" />
+        )}
+      </div>
+
+      <div>
+        <div className="flex justify-between mt-3 text-slate-600 font-medium">
+          <a href={`https://github.com/${github}/`} target="_blank">
+            <p>{github.length > 19 ? `${github.substr(0, 19)}...` : github}</p>
+          </a>
+          <p>{hosted}</p>
+        </div>
+
+        <div className="flex justify-between">
+          <div className="flex gap-2">
+            <a
+              href="#"
+              className="text-slate-400 flex items-center gap-1"
+              onClick={() => setIsOpened((prev) => !prev)}
+            >
+              <MdOutlineOpenInBrowser
+                className={isOpened ? "rotate-180" : ""}
+              />
+            </a>
+            <a
+              href={url}
+              target="_blank"
+              className="text-slate-400 flex items-center gap-1"
+            >
+              <IoOpenOutline />
+            </a>
+          </div>
+          <h3 className="text-slate-600 font-medium">
+            {siteName.length > 20 ? `${siteName.substr(0, 20)}...` : siteName}
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
